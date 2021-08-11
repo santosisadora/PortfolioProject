e23713a7c070e693c6ebb2e7f8330d1a051bbbd3
Isadora-Santos1 <isadora.sass@gmail.com>
2021-08-11 18:58:15 -0400

    1)Added new folder API 2)Inside folder API, created scaffolded Web Api Controller.

---
+++ PortfolioProject/wwwroot/API/FreelanceProjectsApiController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioProject.Data;
+using PortfolioProject.Models;
+
+namespace PortfolioProject.wwwroot.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FreelanceProjectsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FreelanceProjectsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FreelanceProjectsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FreelanceProject>>> GetFreelanceProjects()
+        {
+            return await _context.FreelanceProjects.ToListAsync();
+        }
+
+        // GET: api/FreelanceProjectsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FreelanceProject>> GetFreelanceProject(int id)
+        {
+            var freelanceProject = await _context.FreelanceProjects.FindAsync(id);
+
+            if (freelanceProject == null)
+            {
+                return NotFound();
+            }
+
+            return freelanceProject;
+        }
+
+        // PUT: api/FreelanceProjectsApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFreelanceProject(int id, FreelanceProject freelanceProject)
+        {
+            if (id != freelanceProject.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(freelanceProject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FreelanceProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FreelanceProjectsApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<FreelanceProject>> PostFreelanceProject(FreelanceProject freelanceProject)
+        {
+            _context.FreelanceProjects.Add(freelanceProject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFreelanceProject", new { id = freelanceProject.Id }, freelanceProject);
+        }
+
+        // DELETE: api/FreelanceProjectsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFreelanceProject(int id)
+        {
+            var freelanceProject = await _context.FreelanceProjects.FindAsync(id);
+            if (freelanceProject == null)
+            {
+                return NotFound();
+            }
+
+            _context.FreelanceProjects.Remove(freelanceProject);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FreelanceProjectExists(int id)
+        {
+            return _context.FreelanceProjects.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PortfolioProject/wwwroot/API/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PortfolioProject.wwwroot.API
+{
+    //Endpoint willl be http://localhost:3000/api/Values
+    [Route("api/[controller]")]
+    //Marks this as an API controller
+    [ApiController]
+    //ControllerBase does not offer View support
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
